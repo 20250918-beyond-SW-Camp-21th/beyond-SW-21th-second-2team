0ba252685585421a64ad1b9ee38605290e318650
chaewookim <chaewookim@chaewookimui-MacBookAir.local>
2025-12-01 11:13:42 +0900

    [feat] resource-reservation-service commit

---
+++ resource-reservation-service/src/main/java/com/whatthefork/resourcereservationservice/ResourceReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.whatthefork.resourcereservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceReservationServiceApplication.class, args);
+    }
+
+}
---
+++ resource-reservation-service/src/test/java/com/whatthefork/resourcereservationservice/ResourceReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatthefork.resourcereservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceReservationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
