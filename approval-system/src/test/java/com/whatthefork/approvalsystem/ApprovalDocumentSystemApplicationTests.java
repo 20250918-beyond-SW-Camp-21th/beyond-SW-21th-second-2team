2012fd8507a7820de003a83e905a933312f6b7b9
haengguk <guddnrb14@naver.com>
2025-12-08 14:47:01 +0900

    msa merge

---
+++ approval-system/src/test/java/com/whatthefork/approvalsystem/ApprovalDocumentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whatthefork.approvalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApprovalDocumentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
