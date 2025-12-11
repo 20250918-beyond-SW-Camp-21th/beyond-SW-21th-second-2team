9d05c7d095ea321e00b8c79186d5a64ff52dd6a0
haengguk <guddnrb14@naver.com>
2025-12-11 20:27:25 +0900

    [test] #112 DocumentController 테스트 코드 작성

--- approval-system/src/main/java/com/whatthefork/approvalsystem/dto/request/CreateDocumentRequestDto.java
+++ approval-system/src/main/java/com/whatthefork/approvalsystem/dto/request/CreateDocumentRequestDto.java
@@ -3,6 +3,7 @@ package com.whatthefork.approvalsystem.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
